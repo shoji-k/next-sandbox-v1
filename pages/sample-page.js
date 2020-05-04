2d6decb2260c95650789c8b0f7be4b898bed6dab
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-05-04 15:15:44 +0900

    add title in head

--- pages/sample-page.js
+++ pages/sample-page.js
@@ -1,6 +1,11 @@
+import Head from 'next/head'
+
 export default function SamplePage() {
   return (
     <>
+      <Head>
+        <title>Sample page</title>
+      </Head>
       <h2>sample page</h2>
       <p>hey hey</p>
     </>
