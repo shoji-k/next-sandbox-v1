d96de444e46e74f6b665b35f783690ee1d1a4936
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-05-04 15:24:02 +0900

    write some css

--- pages/sample-page.js
+++ pages/sample-page.js
@@ -2,12 +2,19 @@ import Head from 'next/head'
 
 export default function SamplePage() {
   return (
-    <>
+    <div className="container">
       <Head>
         <title>Sample page</title>
       </Head>
       <h2>sample page</h2>
       <p>hey hey</p>
-    </>
+
+      <style jsx>{`
+        .container {
+          width: 80%;
+          margin: auto;
+        }
+      `}</style>
+    </div>
   )
 }
