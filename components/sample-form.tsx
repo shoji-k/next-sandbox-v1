e06bb4ec5bf7066add6608c31d0f0293ff8417ed
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-05-30 20:14:18 +0900

    add Select

--- components/sample-form.tsx
+++ components/sample-form.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from "react"
-import Text from "./organisms/sample-form/Text"
 import Label from "./organisms/sample-form/Label"
 import Password from "./organisms/sample-form/Password"
+import Select from "./organisms/sample-form/Select"
+import Text from "./organisms/sample-form/Text"
 
 export default function SampleForm(): ReactElement {
   return (
@@ -19,7 +20,7 @@ export default function SampleForm(): ReactElement {
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full px-3">
           <Label inputId="grid-password">Password</Label>
-          <Text id="grid-password" placeholder="******************" />
+          <Password id="grid-password" placeholder="******************" />
           <p className="text-gray-600 text-xs italic">Make it as long and as crazy as you&apos;d like</p>
         </div>
       </div>
@@ -30,16 +31,10 @@ export default function SampleForm(): ReactElement {
         </div>
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
           <Label inputId="grid-state">State</Label>
-          <div className="relative">
-            <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-              <option>New Mexico</option>
-              <option>Missouri</option>
-              <option>Texas</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-            </div>
-          </div>
+          <Select
+            id="grid-state"
+            list={['New Mexico', 'Missouri', 'Texas']}
+          />
         </div>
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
           <Label inputId="grid-zip">Zip</Label>
