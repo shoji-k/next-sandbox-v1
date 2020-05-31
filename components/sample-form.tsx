0b5606ead618d0fa8a23a2ab70bb2be7b436273b
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-05-31 17:01:05 +0900

    add TextLabel

--- components/sample-form.tsx
+++ components/sample-form.tsx
@@ -2,19 +2,17 @@ import React, { ReactElement } from "react"
 import Label from "./organisms/sample-form/Label"
 import Password from "./organisms/sample-form/Password"
 import Select from "./organisms/sample-form/Select"
-import Text from "./organisms/sample-form/Text"
+import TextLabel from "./organisms/sample-form/TextLabel"
 
 export default function SampleForm(): ReactElement {
   return (
     <form className="w-full max-w-lg mx-auto py-8">
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <Label inputId="grid-first-name">First Name</Label>
-          <Text id="grid-first-name" placeholder="Jane" error="Please fill out this field." />
+          <TextLabel id="grid-first-name" placeholder="Jane" error="Please fill out this field.">First Name</TextLabel>
         </div>
         <div className="w-full md:w-1/2 px-3">
-          <Label inputId="grid-last-name">Last Name</Label>
-          <Text id="grid-last-name" placeholder="Doe" />
+          <TextLabel id="grid-last-name" placeholder="Doe">Last Name</TextLabel>
         </div>
       </div>
       <div className="flex flex-wrap -mx-3 mb-6">
@@ -26,8 +24,7 @@ export default function SampleForm(): ReactElement {
       </div>
       <div className="flex flex-wrap -mx-3 mb-2">
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <Label inputId="grid-city">City</Label>
-          <Text id="grid-city" placeholder="Albuquerque" />
+          <TextLabel id="grid-city" placeholder="Albuquerque">City</TextLabel>
         </div>
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
           <Label inputId="grid-state">State</Label>
@@ -37,8 +34,7 @@ export default function SampleForm(): ReactElement {
           />
         </div>
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <Label inputId="grid-zip">Zip</Label>
-          <Text id="grid-zip" placeholder="90210" />
+          <TextLabel id="grid-zip" placeholder="90210">Zip</TextLabel>
         </div>
       </div>
     </form>
