10f495d9ac5cf22343bb1f946272b7f9da09eec0
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-06-13 18:25:01 +0900

    fix Alert

--- components/sample-form.tsx
+++ components/sample-form.tsx
@@ -5,30 +5,35 @@ import Password from './organisms/sample-form/Password'
 import Select from './organisms/sample-form/Select'
 import TextLabel from './organisms/sample-form/TextLabel'
 
-const Alert = (children: React.ReactNode): React.ReactElement => {
-  return (
-    <p className="text-red-500 text-xs italic">{children}</p>
-  )
+type AlertArguments = Readonly<{ children: React.ReactNode }>
+
+const Alert = ({ children }: AlertArguments): React.ReactElement => {
+  return <p className="text-red-500 text-xs italic">{children}</p>
 }
 
 export default function SampleForm(): ReactElement {
-  const { register, errors, handleSubmit, formState} = useForm<FormData>()
+  const { register, errors, handleSubmit, formState } = useForm<FormData>()
   const onSubmit = (data: {}): void => console.log('submit:', data, formState)
 
   return (
-    <form className="w-full max-w-lg mx-auto py-8" onSubmit={handleSubmit(onSubmit)}>
+    <form
+      className="w-full max-w-lg mx-auto py-8"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="flex flex-wrap -mx-3 mb-6">
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <TextLabel
             id="grid-first-name"
             placeholder="Jane"
             register={register}
-            error={!!errors["grid-first-name"]}
+            error={!!errors['grid-first-name']}
           >
             First Name
           </TextLabel>
           <ErrorMessage errors={errors} name="grid-first-name">
-            {({ message }: { message: string }): React.ReactElement => <Alert>{message}</Alert>}
+            {({ message }: { message: string }): React.ReactElement => (
+              <Alert>{message}</Alert>
+            )}
           </ErrorMessage>
         </div>
         <div className="w-full md:w-1/2 px-3">
@@ -36,12 +41,14 @@ export default function SampleForm(): ReactElement {
             id="grid-last-name"
             placeholder="Doe"
             register={register}
-            error={!!errors["grid-last-name"]}
+            error={!!errors['grid-last-name']}
           >
             Last Name
           </TextLabel>
           <ErrorMessage errors={errors} name="grid-last-name">
-            {({ message }: { message: string }): React.ReactElement => <p className="text-red-500 text-xs italic">{message}</p>}
+            {({ message }: { message: string }): React.ReactElement => (
+              <p className="text-red-500 text-xs italic">{message}</p>
+            )}
           </ErrorMessage>
         </div>
       </div>
@@ -52,10 +59,12 @@ export default function SampleForm(): ReactElement {
             id="grid-password"
             placeholder="******************"
             register={register}
-            error={!!errors["grid-password"]}
+            error={!!errors['grid-password']}
           />
           <ErrorMessage errors={errors} name="grid-password">
-            {({ message }: { message: string }): React.ReactElement => <p className="text-red-500 text-xs italic">{message}</p>}
+            {({ message }: { message: string }): React.ReactElement => (
+              <p className="text-red-500 text-xs italic">{message}</p>
+            )}
           </ErrorMessage>
           <p className="text-gray-600 text-xs italic">
             Make it as long and as crazy as you&apos;d like
@@ -68,12 +77,14 @@ export default function SampleForm(): ReactElement {
             id="grid-city"
             placeholder="Albuquerque"
             register={register}
-            error={!!errors["grid-city"]}
+            error={!!errors['grid-city']}
           >
             City
           </TextLabel>
           <ErrorMessage errors={errors} name="grid-city">
-            {({ message }: { message: string }): React.ReactElement => <p className="text-red-500 text-xs italic">{message}</p>}
+            {({ message }: { message: string }): React.ReactElement => (
+              <p className="text-red-500 text-xs italic">{message}</p>
+            )}
           </ErrorMessage>
         </div>
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
@@ -82,10 +93,12 @@ export default function SampleForm(): ReactElement {
             id="grid-state"
             list={['', 'New Mexico', 'Missouri', 'Texas']}
             register={register}
-            error={!!errors["grid-state"]}
+            error={!!errors['grid-state']}
           />
           <ErrorMessage errors={errors} name="grid-state">
-            {({ message }: { message: string }): React.ReactElement => <p className="text-red-500 text-xs italic">{message}</p>}
+            {({ message }: { message: string }): React.ReactElement => (
+              <p className="text-red-500 text-xs italic">{message}</p>
+            )}
           </ErrorMessage>
         </div>
         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
@@ -93,17 +106,23 @@ export default function SampleForm(): ReactElement {
             id="grid-zip"
             placeholder="90210"
             register={register}
-            error={!!errors["grid-zip"]}
+            error={!!errors['grid-zip']}
           >
             Zip
           </TextLabel>
           <ErrorMessage errors={errors} name="grid-zip">
-            {({ message }: { message: string }): React.ReactElement => <p className="text-red-500 text-xs italic">{message}</p>}
+            {({ message }: { message: string }): React.ReactElement => (
+              <p className="text-red-500 text-xs italic">{message}</p>
+            )}
           </ErrorMessage>
         </div>
       </div>
       <div className="flex items-center justify-between">
-        <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Save" />
+        <input
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+          value="Save"
+        />
       </div>
     </form>
   )
