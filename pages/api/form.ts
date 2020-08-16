40ad76219eaba99ebb80649ce9dbbd849b69c736
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-08-16 11:06:35 +0900

    fix

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
+  // console.log(req)
   if (req.method === 'POST') {
     // let req save data
     res.statusCode = 201
-    res.json({ result: "ok", data: req })
+    res.json({ result: "ok" })
   } else {
     res.status(401).json({ message: 'Authentication failed'})
   }
