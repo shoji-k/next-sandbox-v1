3e3c2d2ecd4cb97315e26e8eab130edf28105c21
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-08-17 10:41:28 +0900

    refactor codes for developing

--- pages/api/firebase.ts
+++ pages/api/firebase.ts
@@ -1,27 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import firebase from '@/lib/firebase'
+import { load } from '@/lib/firebase'
 
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  const db = firebase.firestore()
-  return new Promise((resolve, reject) => {
-    db.collection('users')
-      .orderBy('first', 'asc')
-      .limit(10)
-      .get()
-      .then(function (snapshot) {
-        const users = []
-        snapshot.forEach((doc) => {
-          const user = doc.data()
-          users.push({ id: doc.id, ...user })
-        })
-        res.statusCode = 200
-        res.json({ users: users })
-        resolve()
-      }).catch(function(e) {
-        console.log(e)
-        res.statusCode = 500
-        res.json({ errors: [] })
-        reject()
-      })
-  })
+// this codes are for debug to call firebase store api
+export default async function (_req: NextApiRequest, res: NextApiResponse): void {
+  try {
+    const users = await load()
+    res.statusCode = 200
+    res.json({ users: users })
+  } catch (e) {
+    res.statusCode = 500
+    res.json({ errors: [] })
+  }
 }
