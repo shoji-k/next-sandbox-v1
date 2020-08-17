9f146d20c4225e38eb9ceb94fd3b536deb8379db
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-08-17 09:46:41 +0900

    fix warning of firebase fetch

--- pages/api/firebase.ts
+++ pages/api/firebase.ts
@@ -3,24 +3,25 @@ import firebase from '@/lib/firebase'
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
   const db = firebase.firestore()
-  const users = []
-  db.collection('users')
-    .orderBy('first', 'asc')
-    .limit(10)
-    .get()
-    .then(function (snapshot) {
-      if (!snapshot.size) {
-        return
-      }
-      snapshot.forEach((doc) => {
-        const user = doc.data()
-        users.push(user)
+  return new Promise((resolve, reject) => {
+    db.collection('users')
+      .orderBy('first', 'asc')
+      .limit(10)
+      .get()
+      .then(function (snapshot) {
+        const users = []
+        snapshot.forEach((doc) => {
+          const user = doc.data()
+          users.push(user)
+        })
+        res.statusCode = 200
+        res.json({ users: users })
+        resolve()
+      }).catch(function(e) {
+        console.log(e)
+        res.statusCode = 500
+        res.json({ errors: [] })
+        reject()
       })
-      res.statusCode = 200
-      res.json({ users: users })
-    }).catch(function(e) {
-      console.log(e)
-      res.statusCode = 500
-      res.json({ errors: [] })
-    })
+  })
 }
