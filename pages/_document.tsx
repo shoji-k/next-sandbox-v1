b16c8d3aaa7527e113900ef43a97128739c54841
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-09-06 12:27:18 +0900

    set some type

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import React, { ReactElement } from 'react'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
 import theme from '../src/theme'
 
 export default class MyDocument extends Document {
-  render() {
+  render(): ReactElement {
     return (
-      <Html lang="en">
+      <Html lang="ja">
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
@@ -26,7 +32,7 @@ export default class MyDocument extends Document {
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
-MyDocument.getInitialProps = async (ctx) => {
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
   // Resolution order
   //
   // On the server:
--- pages/material-ui.tsx
+++ pages/material-ui.tsx
@@ -1,10 +1,14 @@
 import React, { ReactElement } from 'react'
-import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
 
 export default function MaterialUi(): ReactElement {
   return (
     <Container maxWidth="sm">
+      <Typography variant="h4" component="h1" gutterBottom>
+        Material UI Page
+      </Typography>
       <Box my={4}>hey</Box>
     </Container>
   )
