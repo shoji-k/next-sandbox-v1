54ca5b8f3b974efb666a126026572bbaadb2d32d
kobayashi shoji <shoji.kobayashi.0125@gmail.com>
2020-09-07 17:07:50 +0900

    create form

--- components/organisms/material-ui/form.tsx
+++ components/organisms/material-ui/form.tsx
@@ -1,33 +1,80 @@
 import React, { ReactElement } from 'react'
-import Box from '@material-ui/core/Box'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
+import { useForm } from 'react-hook-form'
+import { Box, Button, Grid, TextField, Typography } from '@material-ui/core'
 
 export default function MaterialUi(): ReactElement {
+  const { register, errors, handleSubmit } = useForm<FormData>()
+  const onSubmit = async (data: {}): Promise<void> => {
+    await console.log('submit', data, errors)
+  }
+
   return (
     <Box pt={1} bgcolor="white">
       <Typography variant="h5" component="h2" gutterBottom>
         Form
       </Typography>
       <Box pb={2}>Input your information.</Box>
-      <form>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <Grid container direction="column" spacing={2}>
           <Grid item>
-            <TextField label="name" variant="outlined" fullWidth required />
+            <TextField
+              label="name"
+              name="name"
+              variant="outlined"
+              fullWidth
+              required
+              error={!!errors.name}
+              helperText={errors.name?.message}
+              inputRef={register({ required: '入力してください' })}
+            />
           </Grid>
           <Grid item>
-            <TextField label="tel" variant="outlined" fullWidth required />
+            <TextField
+              label="tel"
+              name="tel"
+              variant="outlined"
+              placeholder="09000000000"
+              fullWidth
+              required
+              error={!!errors.tel}
+              helperText={errors.tel?.message}
+              inputRef={register({ required: '入力してください' })}
+            />
           </Grid>
           <Grid item>
-            <TextField label="email" variant="outlined" fullWidth />
+            <TextField
+              type="email"
+              label="email"
+              name="email"
+              variant="outlined"
+              fullWidth
+              required
+              placeholder="sample@sample.com"
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              inputRef={register({
+                required: '入力してください',
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: '正しく入力してください',
+                },
+              })}
+            />
           </Grid>
           <Grid item>
-            <TextField label="memo" variant="outlined" fullWidth multiline />
+            <TextField
+              label="memo"
+              name="memo"
+              variant="outlined"
+              fullWidth
+              multiline
+              error={!!errors.memo}
+              helperText={errors.memo?.message}
+              inputRef={register}
+            />
           </Grid>
           <Grid item>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" type="submit">
               Save
             </Button>
           </Grid>
